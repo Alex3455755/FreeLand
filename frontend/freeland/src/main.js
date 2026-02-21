4a93fd2a040fd178f5aa948511dd0ac81ee5c4de
Alex3455755 <alexeigyll@gmail.com>
2026-02-21 22:56:30 +0500

    первые наметки главной страницы

--- frontend/freeland/src/main.js
+++ frontend/freeland/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'  // Путь к вашему роутеру
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)  // Подключение роутера
+  .mount('#app')
