1b6aca4accd4ea4860dff4e550359847026a35fb
Alex3455755 <alexeigyll@gmail.com>
2026-05-25 05:12:46 +0500

    микро доработки

--- frontend/freeland/src/main.js
+++ frontend/freeland/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'  // Путь к вашему роутеру
+import './assets/css/mainStyle.css' // Глобальные стили (загрузчик, скроллбар, переменные)
 
 createApp(App)
   .use(router)  // Подключение роутера
